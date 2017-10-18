144d5e8690ae357602acbd57cac372943fb78f67
Oleg <olegiano12@googlemail.com>
2017-10-18 14:58:26 +0200

    Manager_class_erstellt_first_version

---
+++ src/Manager.java
@@ -0,0 +1,56 @@
+/*
+   Manager Klasse
+ */
+
+public class Manager{
+
+    private final int manID;
+    private Parkhaus manParkhaus;
+    private Widget widgetTool;
+    private double eigeneKasse;
+
+    public Manager(){
+        double zufallsZahl = Math.random();
+        double zufallsZahl2 = Math.random();
+        int ID = zufallsZahl*1333 + zufallsZahl2*133;
+        this.manID = ID;
+        manParkhaus = new Parkhaus(manID);
+        widgetTool = new Widget(manID);
+    }
+
+    void setPreise(double preiseNorm, double preiseNacht, double preiseGanzTag){
+         double[] preise = new double[3];
+         preise[0] = preiseNorm;
+         preise[1] = preiseNacht;
+         preise[2] = preiseGanzTag;
+         manParkhaus.setPreis(preise, manID);
+    }
+
+    void setGeschlossen(boolean statusAendern){
+         if((statusAendern == true) && manParkhaus.getIstGeschlossen() == false){
+             manParkhaus.setGeschlossen(manID);
+         }
+         if((statusAendern == false) && manParkhaus.getIstGeschlossen() == true){
+             man.Parkhaus.setGeoeffnet(manID);
+         }
+    }
+
+    void setOeffnungszeiten(double[7][2] zeiten){
+        manParkhaus.setOeffnungszeiten(zeiten, manID);
+    }
+
+    double getKassenBestand(){
+        return manParkhaus.getKassenBestand(manID);
+    }
+    double takeKassenBestand(int money){
+        double geld = manParkhaus.takeKassenBestand(money, manID);
+        eigeneKasse += geld;
+        return geld;
+    }
+
+    void makeDiagramm(String laenge){
+        arrayList<double[]> liste = manParkhaus.getWidgetList(manID);
+        Widget.makeListe(liste, laenge);
+    }
+
+}
